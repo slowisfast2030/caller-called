f70d0afae408a4cc874be420aaf01a664c65e69d
slowisfast <slowisfast2030@163.com>
2023-05-01 01:13:12 +0800

    slow is fast

--- v3/contracts/AccountFactory.sol
+++ v3/contracts/AccountFactory.sol
@@ -6,10 +6,12 @@ import './UserAccount.sol';
 contract AccountFactory {
   UserAccount public accountOwn;
 
+  // 先执行
   function createAccount() public {
     accountOwn = new UserAccount(msg.sender);
   }
 
+  // 后执行
   function getAccountOwner() public view returns (address) {
     return accountOwn.owner();
   }
